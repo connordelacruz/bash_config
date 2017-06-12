d77f3ebf7b762b1a31f906f6f7a5a29332a67277
connordelacruz <connor.c.delacruz@gmail.com>
2017-06-11 19:17:22 -0500

    Created install script and extracted remaining configurations from .bashrc to sources/config

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # Bash Config
+My custom bash configuration repo.
 
 ## Install Instructions
-// TODO
+Clone repo as ~/.bash_config and run install script:
+```
+git clone https://connordelacruz@bitbucket.org/connordelacruz/bash_config.git ~/.bash_config
+sh ~/.bash_config/install.sh
+```
 
 ## Directory Structure
+// TODO
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+echo 'Installing bash config...'
+
+# Backup existing .bashrc
+if [ -f ~/.bashrc ]; then
+    echo 'Backing up current .bashrc...'
+    cd ~
+    # append timestamp to backup file in case .bashrc.bak already exists
+    timestamp="$(date +'%s')"
+    backup_file=".bashrc.bak.$timestamp"
+    cp .bashrc "$backup_file"
+    echo "Backup of .bashrc created ($backup_file)"
+fi
+
+# ~/.bashrc is a one-liner that sources .bash_config/bash_source
+echo '. ~/.bash_config/bash_source' > ~/.bashrc
+
+# TODO install vimrc stuff
+
+echo 'Bash config installed.'
+echo 'Restarting bash.'
+
+exec bash
