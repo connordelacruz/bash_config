4e5f9ba828e7c92e005f7527614a9836edecc91c
connordelacruz <connor.c.delacruz@gmail.com>
2017-08-15 14:35:45 -0500

    Removed debian-specific stuff from config.sh

--- src_global/config.sh
+++ src_global/config.sh
@@ -4,7 +4,7 @@
 
 
 # ------------------------------------------------------------------------------
-# Stolen from the default Ubuntu .bashrc
+# Terminal configurations
 # ------------------------------------------------------------------------------
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
@@ -23,11 +23,6 @@ shopt -s checkwinsize
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
 
 
 # ------------------------------------------------------------------------------
@@ -70,11 +65,11 @@ if [ -n "$force_color_prompt" ]; then
 fi
 
 if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;92m\]\u@\h\[\033[00m\] \[\033[00;32m\]\w\[\033[02;37m\]\n\$ \[\033[00m\]'
+    PS1='\[\033[01;92m\]\u@\h\[\033[00m\] \[\033[00;32m\]\w\[\033[02;37m\]\n\$ \[\033[00m\]'
     export CLICOLOR=1
     export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
 else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+    PS1='\u@\h:\w\$ '
 fi
 unset color_prompt force_color_prompt
 
@@ -86,7 +81,7 @@ unset color_prompt force_color_prompt
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
 xterm*|rxvt*)
-    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    PS1="\[\e]0;\u@\h: \w\a\]$PS1"
     ;;
 *)
     ;;
