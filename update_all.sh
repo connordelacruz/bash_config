f19ce483a3b3915056d10caf54d76638a5099c09
connordelacruz <connor.c.delacruz@gmail.com>
2017-09-22 17:23:13 -0500

    Modified update.sh to be more verbose and implemented update_all.sh

--- update_all.sh
+++ update_all.sh
@@ -1,2 +1,22 @@
-# TODO: check for .vim_runtime and source update.sh
-# TODO: source update.sh
+#!/bin/bash
+# Call update scripts in personal repos (if they exist)
+
+# Older versions of git don't have the -C option
+current_dir="$(pwd)"
+
+
+# vim_runtime
+if [ -f ~/.vim_runtime/update.sh ]; then
+    . ~/.vim_runtime/update.sh
+fi
+
+# bash_config
+if [ -f ~/.bash_config/update.sh ]; then
+    . ~/.bash_config/update.sh
+fi
+
+cd "$current_dir"
+unset current_dir
+
+echo 'Finished running update scripts.'
+
