5bc6d6bcb60fda5f194314473e8d8d6b056d4a07
connordelacruz <connor.c.delacruz@gmail.com>
2017-09-26 14:42:05 -0500

    Removed setup/ since it will probably go unused.

--- setup/ps1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Script to generate $PS1 colors to override defaults
-
-# TODO: prompt for /u@/h color (and if bold or regular)
-
-# TODO: prompt for /w color (and if bold or regular)
-
