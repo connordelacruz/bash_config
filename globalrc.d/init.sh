c3b8c4a00c34e10717aa5c2a6977e80f99c2efaa
connordelacruz <connor.c.delacruz@gmail.com>
2017-09-26 17:57:08 -0500

    Renamed config.sh to bashrc.sh and moved inputrc export to globalrc.d/init.sh

--- globalrc.d/init.sh
+++ globalrc.d/init.sh
@@ -3,23 +3,11 @@
 # ==============================================================================
 
 
-# Environment variable definitions
-if [ -f "$SRC_GLOBAL_PATH/variables.sh" ]; then
-    . "$SRC_GLOBAL_PATH/variables.sh"
+if [ -f "$SRC_GLOBAL_PATH/inputrc" ]; then
+    export INPUTRC="$SRC_GLOBAL_PATH/inputrc"
 fi
 
-# Bash configuration (PS1, color settings, etc)
-if [ -f "$SRC_GLOBAL_PATH/config.sh" ]; then
-    . "$SRC_GLOBAL_PATH/config.sh"
-fi
-
-# Alias definitions
-if [ -f "$SRC_GLOBAL_PATH/aliases.sh" ]; then
-    . "$SRC_GLOBAL_PATH/aliases.sh"
-fi
-
-# Function definitions
-if [ -f "$SRC_GLOBAL_PATH/functions.sh" ]; then
-    . "$SRC_GLOBAL_PATH/functions.sh"
+if [ -f "$SRC_GLOBAL_PATH/bashrc.sh" ]; then
+    . "$SRC_GLOBAL_PATH/bashrc.sh"
 fi
 
