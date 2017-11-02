ee957290d540cc453bb8cf5a3283f322d97d1bfa
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-11-02 14:28:43 -0500

    Alias colordiff as diff if the command exists and $color_prompt=yes

--- globalrc.d/bashrc.sh
+++ globalrc.d/bashrc.sh
@@ -174,6 +174,10 @@ if [ "$color_prompt" = yes ]; then
     alias fgrep='fgrep --color=auto'
     alias egrep='egrep --color=auto'
     export GREP_COLORS='ms=01;31:mc=01;31:sl=:cx=:fn=35:ln=32:bn=32:se=36'
+    # If colordiff command exists, alias diff
+    if [[ "$(command -v colordiff)" ]]; then
+        alias diff=colordiff
+    fi
 fi
 
 # ------------------------------------------------------------------------------
