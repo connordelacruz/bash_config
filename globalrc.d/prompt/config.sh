061933bc78f4fb4184b6b863e4d2cbbd85b7b969
Connor de la Cruz <cdelacruz@sccadv.com>
2018-10-23 13:07:38 -0500

    Moved prompt variables to config.sh similar to module config

---
+++ globalrc.d/prompt/config.sh
@@ -0,0 +1,26 @@
+# ==============================================================================
+# Configure PS1 Prompt
+# ==============================================================================
+
+# General
+# ----------------
+
+# Prompt symbol
+PS_SYMBOL='\$'
+
+# TODO: Colors
+# ----------------
+
+
+# Powerline
+# ----------------
+
+# Set to 1 to use powerline.sh instead of a standard PS1
+POWERLINE_ENABLE=1
+# Set to 1 to enable git info
+POWERLINE_GIT=1
+# Set to 1 to enable success/failure symbol color changes
+POWERLINE_EXIT_CODE_COLOR=0
+# Set to 1 to show user in powerline
+POWERLINE_SHOW_USER=0
+
--- globalrc.d/prompt/init.sh
+++ globalrc.d/prompt/init.sh
@@ -2,31 +2,11 @@
 # PS1 Configuration
 # ==============================================================================
 
-# ---------------------------------------
-# Configurations
-# ---------------------------------------
+# --------------------------------
+# Import configurations
+# --------------------------------
 
-# General
-# ----------------
-
-# Prompt symbol
-PS_SYMBOL='\$'
-
-# TODO: Colors
-# ----------------
-
-
-# Powerline
-# ----------------
-
-# Set to 1 to use powerline.sh instead of a standard PS1
-POWERLINE_ENABLE=1
-# Set to 1 to enable git info
-POWERLINE_GIT=1
-# Set to 1 to enable success/failure symbol color changes
-POWERLINE_EXIT_CODE_COLOR=0
-# Set to 1 to show user in powerline
-POWERLINE_SHOW_USER=0
+[ -f "$SRC_GLOBAL_PATH/prompt/config.sh" ] && . "$SRC_GLOBAL_PATH/prompt/config.sh"
 
 # ---------------------------------------
 # Source PS1/Powerline
