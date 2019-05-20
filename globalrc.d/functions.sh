4cb8862f48f52a3f47ba3f299c46dc2f19df7f6b
Connor de la Cruz <cdelacruz@sccadv.com>
2019-05-20 15:28:42 -0500

    Cleaned up functions.sh
    - Made syntax more consistent
    - Using local variables
    - Replaced usages of less -r with less -R
    - Extracted common todo operations to _todo-grep

--- globalrc.d/functions.sh
+++ globalrc.d/functions.sh
@@ -4,68 +4,64 @@
 
 # cd into a directory and list its contents
 cdl() {
-    cd "$@"
-    l
+    cd "$@" && l
 }
-# alias it to c
-#(redundant, but cdl is a more informative function name and c is easier to type)
 alias c='cdl'
 
 # Create a directory and cd into it
 mkcd() {
-  mkdir -p "$1"
-  cd "$1"
+  mkdir -p "$1" && cd "$1"
 }
 
 # Use ls if $1 is a directory or less if it's a file (or something else)
+# Defaults to "." to if no argument is provided (i.e. ls .)
 le() {
-    if [ -d "$1" ]; then
-        l "$1"
-    else
-        less "$1"
-    fi
+    local target="${1:-.}"
+    [ -d "$target" ] && l "$target" || less "$target"
 }
 
 
 # Show diff output in less
-
 diff-less() {
-    diff -u $1 $2 | less -r
+    diff -u "$1" "$2" | less -R
 }
 
 
 # Show tree output in less
 tree-less() {
-tree $@ | less -r
+    tree $@ | less -R
 }
 
 
-# TODO: cleanup local vars in functions
+# Helper for todo functions. Usage:
+# _todo-grep <target dir> [<color always>] [<max characters>]
+_todo-grep() {
+    # If arg 2 is > 0, use --color=always (default to auto)
+    local color_arg="auto" && [[ $2 > 0 ]] && color_arg="always"
+    # Set max characters to print per line
+    local N="${3:-80}"
+    grep -RPIino --exclude-dir={.git,.idea,node_modules} --color=$color_arg "(TODO|FIXME).{0,$N}" $1
+}
+
 
 # Print todo/fixme comments in the specified directory
 todo() {
     # If no args are provided, call from current directory
-    proj_dir="${1:-.}"
-    # Set max characters to print per line
-    N=80
-    grep -RPIino --exclude-dir={.git,.idea,node_modules} "(TODO|FIXME).{0,$N}" $proj_dir
+    _todo-grep "${1:-.}"
 }
 
 
 # Show todo/fixme comments in less
-# TODO: remove redundant components
 todo-less() {
     # If no args are provided, call from current directory
-    proj_dir="${1:-.}"
-    # Set max characters to print per line
-    N=80
-    grep -RPIino --exclude-dir={.git,.idea,node_modules} --color=always "(TODO|FIXME).{0,$N}" $proj_dir | less -R
+    _todo-grep "${1:-.}" 1 | less -R
 }
 
 
 # Delete all merged local git branches except master and develop.
 # Prompts for confirmation by default. The -y arg can be used to skip the prompt
 git-branch-cleanup() {
+    local yn
     if [[ $# > 0 ]] && [[ "$1" == "-y" ]]; then
         yn="y"
     else
@@ -78,8 +74,8 @@ git-branch-cleanup() {
         *)
             ;;
     esac
-    unset yn
 }
+
 
 # Print out a timestamp in the format yyyy-mm-dd
 # If the -v flag is used, print verbose timestamp yyyy-mm-dd-HH.MM.SS
