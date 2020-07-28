8549744098c35f733988ec98478e419f69a7155a
Connor de la Cruz <connor.c.delacruz@gmail.com>
2020-07-28 10:21:28 -0500

    Moved functions to bashrc.sh
    - Merged functions.sh with alias section of bashrc
    - l alias is now L
    - l is now an alias for function le

--- globalrc.d/bashrc.sh
+++ globalrc.d/bashrc.sh
@@ -3,8 +3,6 @@
 #
 # Author: Connor de la Cruz
 # Repo: https://github.com/connordelacruz/bash_config
-#
-# TODO overhaul formatting and docblock
 # ==============================================================================
 
 # Environment ==================================================================
@@ -36,7 +34,8 @@ if ! shopt -oq posix; then
     fi
 fi
 # Enable bash completion (MacOS)
-[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
+[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] &&
+    . "/usr/local/etc/profile.d/bash_completion.sh"
 
 # History ----------------------------------------------------------------------
 # Append to the history file, don't overwrite it
@@ -104,18 +103,56 @@ fi
 # https://github.com/junegunn/fzf
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
-# Aliases ======================================================================
+# Aliases and Functions ========================================================
 
 # ls ---------------------------------------------------------------------------
 alias ll='ls -alF'
 alias la='ls -A'
-alias l='ls -CF' # TODO: merge w/ le()
+alias L='ls -CF'
+# Use ls if $1 is a directory or less if it's a file (or something else)
+# Defaults to "." to if no argument is provided (i.e. ls .)
+le() {
+    local target="${1:-.}"
+    [ -d "$target" ] && L "$target" || less "$target"
+}
+alias l='le'
+
+# cd ---------------------------------------------------------------------------
+# Create a directory and cd into it
+mkcd() {
+  mkdir -p "$1" && cd "$1"
+}
+
+# cd into a directory and list its contents
+cdl() {
+    cd "$@" && L
+}
+alias c='cdl'
 
 # git --------------------------------------------------------------------------
 # Quickly push a new branch to remote for the first time
 alias gpush-head='git push -u origin HEAD'
 # Commit all tracked (verbose)
 alias gcommit-av='git commit -av'
+alias gco='gcommit-av'
+
+# Delete all merged local git branches except master and develop.
+# Prompts for confirmation by default. The -y arg can be used to skip the prompt
+git-branch-cleanup() {
+    local yn
+    if [[ $# > 0 ]] && [[ "$1" == "-y" ]]; then
+        yn="y"
+    else
+        read -p "Delete all merged branches except master and develop? (y/[n]): " yn
+    fi
+    case $yn in
+        [yY]* )
+            git branch --no-color | grep -vE "master|develop" | xargs git branch -d
+            ;;
+        *)
+            ;;
+    esac
+}
 
 # Editor -----------------------------------------------------------------------
 alias e="$EDITOR"
@@ -171,4 +208,79 @@ if [[ -n "$COLOR_PROMPT" ]]; then
         alias tree="tree -CF"
     fi
 fi
+
+# grep -------------------------------------------------------------------------
+# Helper for todo functions. Usage:
+# _todo-grep <target dir> [-c] [-n <max characters>]
+_todo-grep() {
+    local targets=
+    local color_arg="auto"
+    local N=80
+    # Get optional args
+    while [ "$1" != "" ]; do
+        case $1 in
+            -c)
+                color_arg="always"
+                ;;
+            -n)
+                # Get value following -n flag
+                # (Or fallback to default if no value was passed)
+                shift
+                N=${1:-$N}
+                ;;
+            *)
+                # Any other arg is a target dir
+                targets="$targets $1"
+                ;;
+        esac
+        shift
+    done
+    grep -RPIino --exclude-dir={.git,.idea,node_modules} --color=$color_arg "(TODO|FIXME).{0,$N}" $targets
+}
+
+# Print todo/fixme comments in the specified file/directory
+todo() {
+    # If no args are provided, call from current directory
+    _todo-grep "${@:-.}"
+}
+
+# less -------------------------------------------------------------------------
+# Show todo/fixme comments in less
+todo-less() {
+    # If no args are provided, call from current directory
+    _todo-grep "${@:-.}" -c | less -R
+}
+
+# Show diff output in less
+diff-less() {
+    diff -u "$1" "$2" | less -R
+}
+
+# Show tree output in less
+tree-less() {
+    tree "$@" | less -R
+}
+
+# Timestamps -------------------------------------------------------------------
+
+# Print out a timestamp in the format yyyy-mm-dd
+# If the -v flag is used, print verbose timestamp yyyy-mm-dd-HH.MM.SS
+# Can be used to quickly print a timestamp to filenames e.g.:
+#   mv "$file" "$(ts)$file"
+ts() {
+    local fmt="+%Y-%m-%d"
+    [[ "$1" == "-v" ]] && fmt="$fmt-%H.%M.%S"
+    date "$fmt"
+}
+
+# Print out the date a file was last modified in the format yyyy-mm-dd
+# TODO support -v similar to ts
+ts-file() {
+    if [[ $# > 0 ]]; then
+        local fmt="+%Y-%m-%d"
+        date -r "$1" "$fmt"
+    else
+        echo "Usage: ts-file <filename>"
+    fi
+}
 
--- init.sh
+++ init.sh
@@ -1,20 +1,19 @@
 # ==============================================================================
 # Sources bash configuration files
 # ==============================================================================
-
 # If not running interactively, don't do anything
 case $- in
     *i*) ;;
     *) return;;
 esac
-
+# RC File Paths ----------------------------------------------------------------
 # Path to shared configuration sources
 export SRC_GLOBAL_PATH="$HOME/.bash_config/globalrc.d"
 # Path to local configuration sources
 export SRC_LOCAL_PATH="$HOME/.bash_config/localrc.d"
 # Path to installed modules
 export SRC_MODULE_PATH="$HOME/.bash_config/modules"
-
+# Source RC Files --------------------------------------------------------------
 # Global configurations
 [ -f "$SRC_GLOBAL_PATH/init.sh" ] && . "$SRC_GLOBAL_PATH/init.sh"
 # Submodules
