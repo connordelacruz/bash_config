f1de35e947259bf259ab9495605839b732d9a781
Connor de la Cruz <connor.c.delacruz@gmail.com>
2021-10-26 10:45:09 -0500

    More colorful chicago skyline on startup

--- globalrc.d/after.sh
+++ globalrc.d/after.sh
@@ -13,12 +13,15 @@ if [ -d "$SRC_MODULE_PATH/chicago-ascii.sh" ]; then
     # Show skyline or flag on startup
     if [ -z "$STARTUP_ASCII_SHOWN" ] || [ "$STARTUP_ASCII_SHOWN" -le 0 ]; then
         if [ $(tput cols) -gt 32 ] && [ $(tput lines) -gt 18 ]; then
+            script_args="-c 5 -c 3 -c 4 -c 1 -c 6 -c 16"
             script_path="$SRC_MODULE_PATH/chicago-ascii.sh/skyline-condensed.sh"
         else
+            script_args=
             script_path="$SRC_MODULE_PATH/chicago-ascii.sh/flag.sh"
         fi
         clear
-        [ -f "$script_path" ] && . "$script_path"
+        [ -f "$script_path" ] && . "$script_path" $script_args
+        unset script_path script_args
         export STARTUP_ASCII_SHOWN=1
     fi
 fi
