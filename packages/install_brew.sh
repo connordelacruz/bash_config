f0f8c94a4a8ab2737af270f67772b80d731c8af6
Connor de la Chrome <connordelachrome@MacBookPro.attlocal.net>
2025-06-25 14:34:11 -0500

    updated leaves n script

--- packages/install_brew.sh
+++ packages/install_brew.sh
@@ -7,11 +7,12 @@ set -e
 # TODO: This is quick and dirty for now cuz I've got a deadline to wipe this
 # computer, but might be nice to streamline this for the future.
 
+# TODO: eh it should be installed before we get here...
 # Download and install brew
-echo "Installing brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "Installed!"
-echo ""
+# echo "Installing brew..."
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo "Installed!"
+# echo ""
 
 # Install packages
 echo "Installing packages in brew-leaves.txt..."
